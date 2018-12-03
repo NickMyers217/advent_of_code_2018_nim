413b02860f141349da8f1bcf3cffb9c889e3e237
nickmyers217 <nickbmyers217@gmail.com>
2018-12-02 18:46:53 -0600

    Refactor: use the nim unittest module for tests

--- tests/day1_test.nim
+++ tests/day1_test.nim
@@ -1,23 +1,34 @@
 import src/day1
 
+import unittest
+
 const testData = """
 +10
 -2
 -1
 """
 
-let parsedInput = parseInput(testData)
-doAssert parsedInput == @[10, -2, -1]
+suite "Day 1 tests":
+  test "Can parse input":
+    let parsedInput = parseInput(testData)
+    check(parsedInput == @[10, -2, -1])
 
-let frequency = calcFrequency(parsedInput)
-doAssert frequency == 7
+  test "Can calculate final frequency":
+    let frequency = calcFrequency(parseInput(testData))
+    check(frequency == 7)
 
-echo("==> Part 1 Answer: ", partOneAnswer("res/day1.txt"))
+  test "Can log the answer for part one":
+    echo("\t==> Part 1 Answer: ", partOneAnswer("res/day1.txt"))
+    require(true)
 
-doAssert firstRecurringFrequency(@[1, -1]) == 0
-doAssert firstRecurringFrequency(@[3, 3, 4, -2, -4]) == 10
-doAssert firstRecurringFrequency(@[-6, 3, 8, 5, -6]) == 5
-doAssert firstRecurringFrequency(@[7, 7, -2, -7, -4]) == 14
+  test "Can find the first recurring frequency":
+    check:
+      firstRecurringFrequency(@[1, -1]) == 0
+      firstRecurringFrequency(@[3, 3, 4, -2, -4]) == 10
+      firstRecurringFrequency(@[-6, 3, 8, 5, -6]) == 5
+      firstRecurringFrequency(@[7, 7, -2, -7, -4]) == 14
 
-echo("==> Part 2 Answer: ", partTwoAnswer("res/day1.txt"))
+  test "Can log the answer for part two":
+    echo("\t==> Part 2 Answer: ", partTwoAnswer("res/day1.txt"))
+    require(true)
 
--- tests/day2_test.nim
+++ tests/day2_test.nim
@@ -1,4 +1,4 @@
-import sequtils
+import unittest, sequtils
 
 import src/day2
 
@@ -23,17 +23,28 @@ const
     "wvxyz"
   ]
 
-doAssert (not testData[0].hasLetterCount(2)) and (not testData[0].hasLetterCount(3))
-doAssert testData[1].hasLetterCount(2) and testData[1].hasLetterCount(3)
+suite "Day 2 tests":
+  test "Can determine if a string has letter count 2 or 3":
+    check:
+      (not testData[0].hasLetterCount(2)) and (not testData[0].hasLetterCount(3))
+      testData[1].hasLetterCount(2) and testData[1].hasLetterCount(3)
 
-doAssert testData.filter(proc(e: string): bool = e.hasLetterCount(2)).len == 4
-doAssert testData.filter(proc(e: string): bool = e.hasLetterCount(3)).len == 3
+  test "Can find the correct number of 2 and 3 letter count strings":
+    check:
+      testData.filter(proc(e: string): bool = e.hasLetterCount(2)).len == 4
+      testData.filter(proc(e: string): bool = e.hasLetterCount(3)).len == 3
 
-doAssert getChecksum(testData) == 12
+  test "Can calculate a correct checksum":
+    check(getChecksum(testData) == 12)
 
-echo("==> Answer Part 1: ", getPartOneAnswer("res/day2.txt"))
+  test "Can log the answer for part one":
+    echo("\t==> Answer Part 1: ", getPartOneAnswer("res/day2.txt"))
+    require(true)
 
-doAssert getTargetBoxLetters(moreTestData) == "fgij"
+  test "Can find the common letters from the correct ids":
+    check(getTargetBoxLetters(moreTestData) == "fgij")
 
-echo("==> Answer Part 2: ", getPartTwoAnswer("res/day2.txt"))
+  test "Can log the answer for part two":
+    echo("\t==> Answer Part 2: ", getPartTwoAnswer("res/day2.txt"))
+    require(true)
 
