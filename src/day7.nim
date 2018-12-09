d06c1438bc4efd3320d6715dc5d145020565ddfa
nickmyers217 <nickbmyers217@gmail.com>
2018-12-09 14:53:36 -0600

    Refactor: clean up day 7 a lot

--- src/day7.nim
+++ src/day7.nim
@@ -1,13 +1,15 @@
-import strscans, strutils, sequtils, tables, algorithm
+import strscans, strutils, sequtils, tables, algorithm, heapqueue
 
 type
   ## A `dependency` that must be fulfilled before `step` can be taken
   Instruction* = tuple[dependency, step: char]
 
+  ## Statuses in a worker's workflow
   Status* = enum IDLE, BUSY, COMPLETE
 
   ## A worker who is accomplishing a task
   Worker* = ref object
+    number: int
     nodeName: char
     status: Status
     timeStarted, duration: int
@@ -24,27 +26,24 @@ func initInstruction*(input: string): Instruction =
   if input.scanf("Step $w must be finished before step $w can begin.", d, s):
     result = (d[0], s[0])
 
-func newWorker*(): Worker =
-  result = Worker()
-
 func `$`*(worker: Worker): string =
+  result = "Worker " & $worker.number & ": "
   case worker.status
   of BUSY:
-    result = "[$1] Started work on $2 at $3. Will finish at $4 seconds" % [
+    result &= "[$1] Started work on $2 at $3. Will finish at $4 seconds" % [
       $worker.status,
       $worker.nodeName,
       $worker.timeStarted,
       $(worker.timeStarted + worker.duration)
     ]
   of IDLE:
-    result = "[$1] Running idle." % [ $worker.status, ]
+    result &= "[$1] Running idle." % [ $worker.status, ]
   of COMPLETE:
-    result = "[$1] Finished work on $2 at $3!" % [
+    result &= "[$1] Finished work on $2 at $3!" % [
       $worker.status,
       $worker.nodeName,
       $(worker.timeStarted + worker.duration)
     ]
-
 
 proc checkIfDone*(worker: var Worker, time: int) =
   ## Updates a worker to COMPLETE status when their task is over
@@ -81,30 +80,27 @@ func kahnsAlgorithm*(graph: Graph): string  =
   result = ""
   var
     graphCopy = graph
-    depsMet: set[char] = {} # The nodes available to be processed
+    depsMet = newHeapQueue[char]() # The nodes available to be processed
 
   # Find all the initially available start nodes
   for key, node in graphCopy.pairs:
-    if node.isStart: depsMet.incl(key)
+    if node.isStart: depsMet.push(key)
 
-  while depsMet != {}:
+  while depsMet.len > 0:
     var
       nodeName: char
       node: NodeValue
 
     # couldnt find a better way to get the first element out of the set :(
-    for n in items[char](depsMet):
-      nodeName = n
-      node = graphCopy[n]
-      depsMet.excl(nodeName)
-      break
+    nodeName = depsMet.pop()
+    node = graphCopy[nodeName]
     result &= nodeName
 
     # Remove the edges between nodeName and its next nodes
     for key in items[char](node.nextNodes):
       graphCopy[key].prevNodes.excl(nodeName)
       # If the node has all its deps met, add it to depsMet
-      if graphCopy[key].prevNodes == {}: depsMet.incl(key)
+      if graphCopy[key].prevNodes == {}: depsMet.push(key)
     node.nextNodes = {}
 
 func kahnsAlgorithmWithScheduling*(
@@ -116,60 +112,57 @@ func kahnsAlgorithmWithScheduling*(
   ## Iterate the dependcy graph using kahn's algorithm
   ## BUT THIS TIME WITH SCHEDULING to find out how long it will take
   var
-    time = 0
-    nodeOrder = ""
+    time = 0 # The current time in seconds
+    finishTimes = newHeapQueue[int]() # Times when tasks will complete
+    nodeOrder = "" # The order tasks were completed
     workers: seq[Worker] = @[]
     graphCopy = graph
-    depsMet: set[char] = {} # The nodes available to be processed
+    depsMet = newHeapQueue[char]() # The nodes available to be processed
 
   # Set up the workers
-  for w in 0 ..< workerCount: workers.add(newWorker())
+  for w in 0 ..< workerCount: workers.add(Worker(number: w))
 
   # Find all the initially available start nodes
   for key, node in graphCopy.pairs:
-    if node.isStart: depsMet.incl(key)
+    if node.isStart: depsMet.push(key)
 
-  proc handleWorker(w: var Worker, number: int) {.closure.} =
+  proc handleWorker(w: var Worker) {.closure.} =
+    ## Step a worker through his workflow
     if w.status == IDLE:
-      if debug: echo "Worker ", number, ": ", w
-      for n in items[char](depsMet):
-        workers[number].assign(n, time, baseTaskTime)
-        depsMet.excl(n)
-        break
+      if debug: echo w
+      if depsMet.len > 0:
+        w.assign(depsMet.pop(), time, baseTaskTime)
+        finishTimes.push(w.timeStarted + w.duration)
     if w.status == BUSY:
-      if debug: echo "Worker ", number, ": ", w
-      workers[number].checkIfDone(time)
+      if debug: echo w
+      w.checkIfDone(time)
     if w.status == COMPLETE:
-      if debug: echo "Worker ", number, ": ", w
+      if debug: echo w
       for key in items[char](graphCopy[w.nodeName].nextNodes):
         graphCopy[key].prevNodes.excl(w.nodeName)
-        if graphCopy[key].prevNodes == {}:
-          depsMet.incl(key)
-      if debug: echo "\t=> Tasks: ", depsMet
+        if graphCopy[key].prevNodes == {}: depsMet.push(key)
       graphCopy[w.nodeName].nextNodes = {}
       nodeOrder &= w.nodeName
-      workers[number].status = IDLE # ready to work!
-      handleWorker(w, number) # go around again and snatch up the next task
+      w.status = IDLE # ready to work!
 
-  while workers.filter(proc(w: Worker): bool = w.status == BUSY).len > 0 or depsMet != {}:
-    if debug: echo "Time: ", time, " Tasks: ", depsMet
+  while workers.filterIt(it.status == BUSY).len > 0 or depsMet.len > 0:
+    if debug: echo "Time: ", time
+
+    # Handle each worker
     for i in workers.low .. workers.high:
-      handleWorker(workers[i], i)
-    # Double check and make sure there are no idle workers that could pick up tasks,
+      handleWorker(workers[i])
+
+    # Make sure there are no idle workers that could pick up newly created tasks,
     # sometimes the last worker frees up a bunch of stuff when the earlier ones were
     # idle with nothing to do
-    for i in workers.low .. workers.high:
-      if workers[i].status == IDLE: handleWorker(workers[i], i)
-    if debug: echo "\n"
-    var times = workers
-      .filter(proc(w: Worker): bool = w.status == BUSY)
-      .map(proc(w: Worker): int = w.timeStarted + w.duration)
-      .sorted(cmp[int])
-    # step to the next time something will finish
-    if times.len > 0: time = times[0]
+    while depsMet.len > 0 and workers.filterIt(it.status == IDLE).len > 0:
+      for i in workers.low .. workers.high:
+        if workers[i].status == IDLE: handleWorker(workers[i])
+
+    # Step to the next time something will finish
+    if finishTimes.len > 0: time = finishTimes.pop()
     else: inc time
 
-  if debug: echo nodeOrder
   result = time - 1
 
 proc printAnswers*(filePath: string, debug: bool = false) =
@@ -179,19 +172,20 @@ proc printAnswers*(filePath: string, debug: bool = false) =
       .filter(proc(e: string): bool = e != "")
     instructions = input.map(initInstruction)
     graph = initGraph(instructions)
-    output = graph.kahnsAlgorithm()
+    part1 = graph.kahnsAlgorithm()
 
   if debug:
     for key, val in graph.pairs: echo "{ ", key, ": ", val, "}"
     echo "\n============================================\n"
-    for c in output: echo "{ ", c, ": ", graph[c].nextNodes, " }"
+    for c in part1: echo "{ ", c, ": ", graph[c].nextNodes, " }"
     echo "\n============================================\n"
 
-  echo output
+  let part2 = graph.kahnsAlgorithmWithScheduling(5, 60, debug)
 
   if debug: echo "\n============================================\n"
 
-  echo graph.kahnsAlgorithmWithScheduling(5, 60, debug)
+  echo part1
+  echo part2
 
 when isMainModule:
   printAnswers("res/day7.txt", true)
