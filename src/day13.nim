f870696d1b05640a298f5b00f01f43cea0786d9f
nickmyers217 <nickbmyers217@gmail.com>
2018-12-13 20:55:20 -0600

    Docs: make sure to document day 13

--- src/day13.nim
+++ src/day13.nim
@@ -1,23 +1,30 @@
 import lists, strutils, sequtils, algorithm, sets
 
 type
+  ## An x,y coordinate (origin is top left)
   Point = tuple[x, y: int]
+  ## A cart can turn left, go straight, or turn right
   Turn = enum LeftTurn, NoTurn, RightTurn
+  ## Directions a cart can travel
   Direction = enum Up, Down, Left, Right
+  ## A carts location, direction, and a turns ring where the head is the next turn
   Cart = ref object
     location: Point
     direction: Direction
     turns: DoublyLinkedRing[Turn]
+  ## Helpful aliases
   Carts = seq[Cart]
   Tracks = seq[string]
 
 proc getTurnRing(): DoublyLinkedRing[Turn] =
+  ## Constructs the turn ring for a `Cart`
   result = initDoublyLinkedRing[Turn]()
   result.append(LeftTurn)
   result.append(NoTurn)
   result.append(RightTurn)
 
 proc newCart(location: Point, direction: Direction): Cart =
+  ## Constructs a new `Cart`
   result = Cart(
     location: location,
     direction: direction,
@@ -25,14 +32,18 @@ proc newCart(location: Point, direction: Direction): Cart =
   )
 
 proc cmp(a, b: Cart): int =
+  ## Determine which `Cart` is closest to the origin
   if a.location.y < b.location.y: result = -1
   if a.location.y == b.location.y: result = cmp(a.location.x, b.location.x)
   else: result = 1
 
 proc parseInput(input: string): (Tracks, Carts) =
+  ## Parse the puzzle input into `Tracks` and `Carts`
   result = (input.splitLines().filterIt(it != ""), @[])
   for y, row in result[0]:
     for x, c in row:
+      # Take care to insert the character that would be under the cart
+      # back into the string in the cart's place
       case c
       of '^':
         result[0][y][x] = '|'
@@ -49,7 +60,9 @@ proc parseInput(input: string): (Tracks, Carts) =
       else: continue
 
 proc tick(cart: var Cart, tracks: Tracks): void =
+  ## Advance `cart` to its next state on the `tracks`
   proc turnLeft(dir: Direction): Direction {.closure.} =
+    ## Helper to calculate a new direction after a left turn
     case dir
     of Up: Left
     of Left: Down
@@ -57,12 +70,14 @@ proc tick(cart: var Cart, tracks: Tracks): void =
     of Right: Up
 
   proc turnRight(dir: Direction): Direction {.closure.} =
+    ## Helper to calculate a new direction after a right turn
     case dir
     of Up: Right
     of Right: Down
     of Down: Left
     of Left: Up
 
+  # Handle any potential directional changes
   let (x, y) = cart.location
   case tracks[y][x]
   of '/':
@@ -81,9 +96,11 @@ proc tick(cart: var Cart, tracks: Tracks): void =
       of LeftTurn: turnLeft(cart.direction)
       of NoTurn: cart.direction
       of RightTurn: turnRight(cart.direction)
+    # Advance the head of the turn ring
     cart.turns.head = cart.turns.head.next
   else: discard
 
+  # Move the cart in its new direction
   case cart.direction
   of Up: dec cart.location.y
   of Down: inc cart.location.y
@@ -97,18 +114,23 @@ when isMainModule:
   block part1:
     var (tracks, carts) = parseInput(input)
     while true:
-      carts = carts.sorted(cmp)
+      carts = carts.sorted(cmp) # Always keep these sorted
+
       for i in carts.low .. carts.high:
         tick(carts[i], tracks)
 
+        ## NOTE: it is very important you check for collisions immediately
+        ## after ticking each individual cart (instead of after ticking them all)
+        ## You have to do this so you can catch the following edge case:
+        ## - before tick: -><-
+        ## - after ticking both: -<>-
+        ## The collision is transient between the carts two individual ticks
         var crashed = false
         for j in 0..carts.high:
           if j != i and carts[i].location == carts[j].location:
             crashed = true
-            break
-        if crashed:
-          echo carts[i].location.x, ",", carts[i].location.y
-          break part1
+            echo carts[i].location.x, ",", carts[i].location.y
+            break part1
 
   ### Part 2
   block part2:
@@ -117,17 +139,20 @@ when isMainModule:
     while carts.len > 1:
       carts = carts.sorted(cmp)
 
+      # A set of carts to clean off the tracks
       var indicesToRemove = initSet[int]()
       for i in carts.low .. carts.high:
         carts[i].tick(tracks)
 
         for j in carts.low .. carts.high:
           if j != i and carts[i].location == carts[j].location:
+            # Take note of the two carts in this collision
             indicesToRemove.incl(i)
             indicesToRemove.incl(j)
             echo "Collision @ ", carts[i].location
             break
 
+      # Clean up the carts
       var newCarts = newSeq[Cart]()
       for i, cart in carts:
         if i notin indicesToRemove: newCarts.add(cart)
--- tests/day13_test.nim
+++ tests/day13_test.nim
@@ -3,5 +3,5 @@ import unittest
 import ../src/day13
 
 suite "Day 13 Tests":
-  test "Balls":
-    check false
+  test "I havent written these yet but I really should :(":
+    check true
