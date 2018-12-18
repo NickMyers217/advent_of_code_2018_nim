a0f299d24edba7927485cc7e6b1b3ec4b8ad59e7
nickmyers217 <nickbmyers217@gmail.com>
2018-12-18 01:49:07 -0600

    Feat: finished day 15 part 1!
    I was having to much difficulty getting A*
    to work for every single scenario :(
    I decied that algorithm was "too smart" and swapped it
    out for breadth first search, which fixed everything...

--- src/day15.nim
+++ src/day15.nim
@@ -1,4 +1,4 @@
-import algorithm, heapqueue, tables, strutils, sequtils, sets
+import algorithm, heapqueue, queues, tables, strutils, sequtils, sets
 
 type
   ## A 2d vector for points and directions
@@ -29,29 +29,18 @@ proc waitForUser(prompt: string): bool {.inline.} =
   var input = readLine(stdin)
   result = input.strip().toLowerAscii() == "y"
 
-proc `+`(a, b: Vec): Vec =
-  ## Vector addition
-  result = (y: a.y + b.y, x: a.x + b.x)
-
-proc `-`(a, b: Vec): Vec =
-  ## Vector subtraction
-  result = (y: a.y - b.y, x: a.x - b.x)
-
-proc manhattan(a, b: Vec): int =
-  ## Manhattan distance between 2 vecs
-  result = abs(a.y - b.y) + abs(a.x - b.x)
-
-proc `==`(a, b: Vec): bool =
-  result = a.y == b.y and a.x == b.x
-
-proc `<`(a, b: Vec): bool =
-  result = if a.y < b.y: true
-    elif a.y == b.y: a.x < b.x
-    else: false
+## Vector helpers
+proc `+`(a, b: Vec): Vec {.inline.} = (y: a.y + b.y, x: a.x + b.x)
+proc `-`(a, b: Vec): Vec {.inline.} = (y: a.y - b.y, x: a.x - b.x)
 
 proc newEntity(pos: Vec, team: Team): Entity =
   ## Construct a new `Entity`
   result = Entity(pos: pos, hp: 200, ap: 3, team: team)
+
+proc `$`*(entity: Entity): string =
+  result = case entity.team
+  of Elf: "E"
+  of Goblin: "G"
 
 iterator neighbors(vec: Vec, game: Game): Vec =
   ## Iterate a location `Vec` for each cell that neighbors `vec` on `game`'s grid
@@ -81,13 +70,7 @@ proc newGame*(input: string): Game =
         entities.add (y, x), newEntity((y, x), Goblin)
         inc teamCounts.goblins
       else: discard
-
   result = Game(grid: grid, entities: entities, round: 0, teamCounts: teamCounts)
-
-proc `$`*(entity: Entity): string =
-  result = case entity.team
-  of Elf: "E"
-  of Goblin: "G"
 
 proc `$`*(game: Game): string =
   result = ""
@@ -118,74 +101,31 @@ proc renderWithHpInfo*(game: Game, pointsToMark: seq[(Vec, char)] = @[]): string
     lines[point[0].y][point[0].x] = point[1]
   result = lines.join("\n")
 
-proc newPosHeap(entities: Table[Vec, Entity]): HeapQueue[Vec] =
+proc debugRender*(game: Game, pointsToMark: seq[(Vec, char)] = @[], wait = true) =
+  ## Helper to render the game for debugging/interactive mode
+  debugEcho game.renderWithHpInfo(pointsToMark)
+  debugEcho "************************************"
+  if wait: waitForUser()
+
+proc newPosHeap(entities: Table[Vec, Entity]): HeapQueue[Vec] {.inline.} =
   ## Make a new heap queue out of an entities table
   result = newHeapQueue[Vec]()
   for pos in entities.keys:
     result.push pos
 
-proc pathFind(game: Game, start, goal: Vec, debug = false): seq[Vec] =
-  ## An implementation of the A* algorithm for efficient pathfinding
-  ## Returns the shortest best reading-order seq of locations that need
+proc pathFind(game: Game, start, goal: Vec): seq[Vec] =
+  ## An implementation of breadth first search for pathfinding
+  ## returns the shortest best reading-order seq of locations that need
   ## to be visited to get from `start` to `goal`, or an empty seq
   ## if `goal` cannot be reached
   var
+    openSet = initQueue[Vec]() # The cells we have discovered for checking
     closedSet = initSet[Vec]() # The cells we have checked already
-    openSet = initSet[Vec]() # The cells we have discovered for checking
     cameFrom = initTable[Vec, Vec]() # A lookup to find the previous location of a cell
-    gScore = initTable[Vec, int]() # A lookup for a cell's gScore (the # of steps taken)
-    fScore = initTable[Vec, int]() # A lookup for a cell's fScore (see the heuristic)
 
-  proc heuristic(a, b: Vec): int {.closure.}  =
-    ## A heuristic to determine the fScore of a and b
-    ## the fScore should be an estimate of best-case  distance left to the goal
-    ##
-    ## NOTE: It is super important that you catch this edge case:
-    ##
-    ##  Moving the goblin on (1, 4) to (5, 5):
-    ##  #######
-    ##  #.0G..# <-- 0 need to path find for (y: 1, x: 3)
-    ##  #..1G.# <-- 1 need to path find for (y: 2, x: 4)
-    ##  #.#G#G#
-    ##  #...#E#
-    ##  #....d# <-- d trying to get here (y: 5, x: 5)
-    ##  #######
-    ##
-    ## If you just take the manhattan distance from (1, 3) to (5, 5)
-    ## and compare it to the manhattan of (2, 4) and (5, 5) you will
-    ## see that (2, 4) has a lower score; even though they are actually
-    ## the same number of steps from (5, 5) AND (1, 3) is first in reading
-    ## order. We really needed to take just the manhattan from (1, 4)
-    ## for both, so that the fScore could tie and be resolved by the heap
-    ## queue later on.
-    result =
-      if cameFrom.hasKey(a): manhattan(cameFrom[a], b)
-      else: manhattan(a, b)
-
-  openSet.incl(start)
-  gscore.add(start, 0)
-  fscore.add(start, heuristic(start, goal))
-
+  openSet.enqueue(start)
   while openSet.len > 0:
-    # This queue will always pop the vec with the best reading order first
-    var
-      smallestFScore = -1
-      next = newHeapQueue[Vec]()
-    for vec in openSet.items:
-      if smallestFScore == -1 or fscore[vec] < smallestFScore:
-        smallestFScore = fscore[vec]
-    for vec in openSet.items:
-      if fScore[vec] == smallestFScore: next.push(vec)
-
-    if debug:
-      debugEcho "Dumping nextQueue:"
-      var queueStr: seq[string] = @[]
-      for i in 0 ..< next.len:
-        queueStr.add $(next[i]) & " => " & $fScore[next[i]]
-      debugEcho queueStr.join(", ")
-
-    # The first item in the queue will be the lowest fScore, and the best reading-order
-    var current = next.pop()
+    var current = openSet.dequeue()
 
     # We made it, so build up the seq of steps to return
     if current == goal:
@@ -195,34 +135,20 @@ proc pathFind(game: Game, start, goal: Vec, debug = false): seq[Vec] =
         result = @[current].concat(result)
       return result[1 .. result.high] # Exclude the start point from the steps
 
-    # Move this cell from the openSet to the closedSet since we are processing it now
-    openSet.excl(current)
-    closedSet.incl(current)
-
+    # Process the available neighbors
     for neighbor in current.neighbors(game):
       let (y, x) = neighbor
-      if game.grid[y][x] != Open or closedSet.contains(neighbor):
+      if game.grid[y][x] != Open or neighbor in closedSet:
         # Don't check this cell, it is a waste of effort
         continue
-
-      let tempGScore = gScore[current] + 1
-
       if neighbor notin openSet:
-        openSet.incl(neighbor) # We just discovered a new cell
-      elif tempGScore >= gScore[neighbor]:
-        # This neighbor was discovered earlier and had a better gScore
-        # so lets continue for now and come back to this laster
-        continue
+        # We just discovered a new cell
+        if cameFrom.hasKeyOrPut(neighbor, current):
+          cameFrom[neighbor] = current
+        openSet.enqueue(neighbor)
 
-      # Update the lookups
-      if cameFrom.hasKeyOrPut(neighbor, current):
-        cameFrom[neighbor] = current
-
-      let tempFScore = tempGScore + heuristic(neighbor, goal)
-      if gScore.hasKeyOrPut(neighbor, tempGScore):
-        gScore[neighbor] = tempGScore
-      if fScore.hasKeyOrPut(neighbor, tempFScore):
-        fScore[neighbor] = tempFScore
+    # Move this cell to the closedSet since we are done processing it now
+    closedSet.incl(current)
 
 proc getNextMove(
   game: Game,
@@ -242,13 +168,10 @@ proc getNextMove(
       if game.grid[y][x] == Open:
         destinations.add(pos)
 
-  if debug: debugEcho "==>Destinations: ", destinations
+  if debug:
+    debugEcho "==>Destinations: ", destinations
   if interactive:
-    debugEcho game.renderWithHpInfo(
-      @[(entity.pos, 'M')].concat destinations.mapIt((it, '?'))
-    )
-    debugEcho "************************************"
-    waitForUser()
+    game.debugRender(@[(entity.pos, 'M')].concat destinations.mapIt((it, '?')))
 
   # Nowhere to go, the move is (0, 0)
   if destinations.len == 0:
@@ -259,18 +182,15 @@ proc getNextMove(
     paths = initTable[Vec, seq[Vec]]()
     reachable: seq[Vec] = @[]
   for vec in destinations:
-    let path = game.pathFind(entity.pos, vec, false)
+    let path = game.pathFind(entity.pos, vec)
     if path.len > 0:
       paths.add(vec, path)
       reachable.add(vec)
 
-  if debug: debugEcho "==>Reachable: ", reachable
+  if debug:
+    debugEcho "==>Reachable: ", reachable
   if interactive:
-    debugEcho game.renderWithHpInfo(
-      @[(entity.pos, 'M')].concat reachable.mapIt((it, '@'))
-    )
-    debugEcho "************************************"
-    waitForUser()
+    game.debugRender(@[(entity.pos, 'M')].concat reachable.mapIt((it, '@')))
 
   # Nothing is reachable, the move is (0, 0)
   if reachable.len ==  0:
@@ -278,31 +198,21 @@ proc getNextMove(
 
   # 3) Find the reachable points with the smallest distance
   var
-    smallestLen: int
+    smallestLen = high(int)
     nearest = newHeapQueue[Vec]() # This heap queue will handle ties
   for vec in reachable:
-    if smallestLen == 0 or paths[vec].len <= smallestLen:
-      smallestLen = paths[vec].len
+    if paths[vec].len <= smallestLen: smallestLen = paths[vec].len
   for vec in reachable:
-    if paths[vec].len == smallestLen:
-      nearest.push(vec)
+    if paths[vec].len == smallestLen: nearest.push(vec)
 
-  var points = newSeq[(Vec, char)]()
   if debug:
-    if entity.pos == (26, 23):
-      for p in paths.pairs:
-        if p[1].len < 22:
-          debugEcho "Can reach ", p[0], " in ", p[1].len, " steps => ", p[1], "\n"
-          points.add((p[0], 'c'))
-          for v in p[1]: points.add((v, 'r'))
     for n in 0 ..< nearest.len:
       debugEcho "==>Nearest: ", nearest[n], " in ", smallestLen, " steps"
   if interactive:
+    var points = newSeq[(Vec, char)]()
     for n in 0 ..< nearest.len:
       points.add((nearest[n], char(n + 48)))
-    debugEcho game.renderWithHpInfo(@[(entity.pos, 'M')].concat(points))
-    debugEcho "************************************"
-    waitForUser()
+    game.debugRender(@[(entity.pos, 'M')].concat(points))
 
   # 4) Choose the smalest reachable point with the best reading-order,
   # take the direction of the first move on the path there
@@ -347,7 +257,6 @@ proc advanceRound*(game: var Game, debug = false, interactive = false): void =
 
   # Queue up the entities by reading-order
   var posQueue = newPosHeap(game.entities)
-
   while posQueue.len > 0:
     let nextPos = posQueue.pop()
 
@@ -366,15 +275,13 @@ proc advanceRound*(game: var Game, debug = false, interactive = false): void =
     if target != nil:
       # We are in range to attack
       game.attack(entity, target)
-
       if debug:
         debugEcho "=>Attack first: ", target.pos
-        debugEcho game.renderWithHpInfo(@[(entity.pos, 'A'), (target.pos, 'T')])
-        debugEcho "************************************"
-      if interactive: waitForUser()
+        game.debugRender(@[(entity.pos, 'A'), (target.pos, 'T')], false)
+      if interactive:
+        waitForUser()
     else:
       if debug: debugEcho "=>Nothing to attack..."
-
       # Check if combat is over
       var combatEnded = true
       for e in game.entities.values:
@@ -388,19 +295,19 @@ proc advanceRound*(game: var Game, debug = false, interactive = false): void =
       # Try to move
       if debug:
         debugEcho "=>Path Finding..."
-        debugEcho game.renderWithHpInfo(@[(entity.pos, 'M')])
-        debugEcho "************************************"
+        game.debugRender(@[(entity.pos, 'M')], false)
       var interactivePathFind = false
       if interactive:
         interactivePathFind = waitForUser("Show path-finding process? (y/n)...")
 
-      let (delta, path) = game.getNextMove(entity, debug, interactivePathFind)
+      let (delta, path) = game.getNextMove(entity, interactivePathFind, interactivePathFind)
 
       if debug:
         debugEcho "=>Moving by ", delta
-        debugEcho game.renderWithHpInfo(@[(entity.pos, 'M')].concat(path.mapIt((it, 'o'))))
-        debugEcho "************************************"
-      if interactive: waitForUser()
+        game.debugRender(@[(entity.pos, 'M')].concat(path.mapIt((it, 'o'))), false)
+      if interactive:
+        waitForUser()
+
       game.move(entity, delta)
 
       # Try and attack again after our move
@@ -409,9 +316,9 @@ proc advanceRound*(game: var Game, debug = false, interactive = false): void =
         game.attack(entity, target)
         if debug:
           debugEcho "=>Attack last: ", target.pos
-          debugEcho game.renderWithHpInfo(@[(entity.pos, 'A'), (target.pos, 'T')])
-          debugEcho "************************************"
-        if interactive: waitForUser()
+          game.debugRender(@[(entity.pos, 'A'), (target.pos, 'T')], false)
+        if interactive:
+          waitForUser()
 
     if debug:
       debugEcho "[End]"
@@ -419,8 +326,7 @@ proc advanceRound*(game: var Game, debug = false, interactive = false): void =
 
   if debug:
     debugEcho "AFTER ROUND: ", game.round
-    debugEcho game.renderWithHpInfo()
-    debugEcho "************************************"
+    game.debugRender(@[], false)
 
 proc tallyScore*(game: Game): int =
   ## Calcuate the product of the full rounds played and
@@ -439,13 +345,6 @@ proc play*(game: var Game, debug = false, interactive = false): void =
 when isMainModule:
   let input = readFile("res/day15.txt")
   var game = newGame(input)
-
-  #let
-  #  entity = game.entities[(26, 23)]
-  #  path = game.pathFind(entity.pos, (25, 11), true)
-  #echo path
-  #debugEcho game.renderWithHpInfo(path.mapIt((it, 'o')))
-  #debugEcho "************************************"
 
   echo game
   game.play(true, false)
