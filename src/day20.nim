f02cbc8efae2ecc81db356d9e89ef66d8dfc57a6
nickmyers217 <nickbmyers217@gmail.com>
2018-12-20 01:47:53 -0600

    Docs: document day 20

--- src/day20.nim
+++ src/day20.nim
@@ -1,15 +1,21 @@
 import strutils, tables, deques, sets, sequtils
 
 type
+  ## 2d vectors (really tired of making this type every day lol)
   Vec = tuple[x, y: int]
+  ## The types of edges on a graph
   Edge = enum Wall Door
+  ## A node on a graph is just 4 edges
   Node = tuple
     north, south, east, west: Edge
+  ## A graph is just a lookup of nodes by their coordinates (relative to the origin)
   Graph = Table[Vec, Node]
+  ## A facility is a start position, a set of instructions to generate a graph, and the graph
   FacilityMap = object
     startPos: Vec
     instructions: string
     graph: Graph
+
 ## Vector math helpers
 proc `-`(a, b: Vec): Vec {.inline.} = (x: a.x - b.x, y: a.y - b.y)
 proc `+`(a, b: Vec): Vec {.inline.} = (x: a.x + b.x, y: a.y + b.y)
@@ -17,57 +23,66 @@ proc north(a: Vec): Vec {.inline.} = a - (0,1)
 proc south(a: Vec): Vec {.inline.} = a + (0,1)
 proc east(a: Vec): Vec {.inline.} = a + (1,0)
 proc west(a: Vec): Vec {.inline.} = a - (1,0)
-#proc `<`(a, b: Vec): bool {.inline.} = (if a.y == b.y: a.x < b.x else: a.y < b.y)
-#proc `*`(a: int, b: Vec): Vec {.inline.} = (x: a * b.x, y: a * b.y)
-#proc inc(a: var Vec, b: Vec) {.inline.} = a = a + b
-#proc dec(a: var Vec, b: Vec) {.inline.} = a = a - b
 iterator neighbors(a: Vec): Vec =
+  ## iterate the neighbors lazy man style
   yield a.north
   yield a.east
   yield a.west
   yield a.south
 
 proc initGraph(instructions: string, startPos: Vec = (0, 0)): Graph =
+  ## Generate a graph from a set of instructions using an explicit stack to iterate the different branches
   result = initTable[Vec, Node]()
   var
-    posStack: seq[Vec] = @[]
-    curPos = startPos
+    posStack: seq[Vec] = @[] # The locations we branched
+    curPos = startPos # Our current location
   for c in instructions:
     echo "Current at ", curPos, " processing ", c, " with stack ", posStack
     case c
     of '^':
+      # the start
       result.add(startPos, (Wall, Wall, Wall, Wall))
     of 'N':
+      # Make sure there is a node north with the proper edges
       result[curPos].north = Door
       curPos = curPos.north
       if result.hasKeyOrPut(curPos, (north: Wall, south: Door, east: Wall, west: Wall)):
         result[curPos].south = Door
     of 'S':
+      # Make sure there is a node south with the proper edges
       result[curPos].south = Door
       curPos = curPos.south
       if result.hasKeyOrPut(curPos, (north: Door, south: Wall, east: Wall, west: Wall)):
         result[curPos].north = Door
     of 'E':
+      # Make sure there is a node east with the proper edges
       result[curPos].east = Door
       curPos = curPos.east
       if result.hasKeyOrPut(curPos, (north: Wall, south: Wall, east: Wall, west: Door)):
         result[curPos].west = Door
     of 'W':
+      # Make sure there is a node west with the proper edges
       result[curPos].west = Door
       curPos = curPos.west
       if result.hasKeyOrPut(curPos, (north: Wall, south: Wall, east: Door, west: Wall)):
         result[curPos].east = Door
     of '(':
+      # We branched, add a new location to the stack
       posStack.add(curPos)
-    of ')':
-      curPos = posStack.pop()
     of '|':
+      # Reset to the start of this branch to test the next possibility
       curPos = posStack[^1]
+    of ')':
+      # This branch is over, pop back
+      curPos = posStack.pop()
     of '$':
+      # The end
       return result
     else: discard
 
 proc doorBetween(graph: Graph, a, b: Vec): bool =
+  ## Return true if there are door edges between adjacent nodes `a` and `b` on `graph`
+  ## otherwise return false
   result = false
   if graph.hasKey(a) and graph.hasKey(b):
     let delta = b - a
@@ -92,12 +107,13 @@ proc breadthFirstSearch(graph: Graph, start, goal: Vec): seq[Vec] =
     let current = openSet.popFirst()
 
     if current == goal:
+      # We found the path, reconstruct it
       result = @[goal]
       var temp = current
       while cameFrom.hasKey(temp):
         temp = cameFrom[temp]
         result = @[temp].concat(result)
-      return result[1..^1]
+      return result[1..^1] # Don't include the start node's positin
 
     for n in current.neighbors:
       if not graph.hasKey(n) or n in closedSet or (not graph.doorBetween(current, n)):
@@ -109,6 +125,7 @@ proc breadthFirstSearch(graph: Graph, start, goal: Vec): seq[Vec] =
     closedSet.incl(current)
 
 proc initFacilityMap(instructions: string, startPos: Vec = (0, 0)): FacilityMap =
+  ## Construct a FacilityMap
   result = FacilityMap(
     startPos: startPos,
     instructions: instructions,
